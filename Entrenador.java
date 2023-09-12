202fa9f2d7e7e2b1ad0a3c45b077f0684ff1650b
EzequielLassalle <92544769+EzequielLassalle@users.noreply.github.com>
2023-09-11 23:01:38 -0300

    Add files via upload

---
+++ Entrenador.java
@@ -0,0 +1,39 @@
+import java.util;
+
+
+public class Entrenador{
+    
+    String nombre;
+    List <Pokemon> pokemones = new Arraylist<Pokemon>();
+    List<Items> inventario = new ArrayList<Items>();
+    Pokemon pokemonActual;
+
+
+    public void cambiarPokemon(int proximo){
+
+    }
+
+    public void  curarPokemon(int efectividad){
+
+    }
+
+    public void restaurarVidaPokemon(){
+
+    }
+
+    public Boolean usarItem(String unItem){
+
+    }
+
+    public void revivirPokemon(){
+
+    }
+
+
+
+
+
+
+
+
+}
