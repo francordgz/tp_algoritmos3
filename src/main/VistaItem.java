e44633eb9ec9463f59bc638d87d1501780540500
facubaratta <fbaratta@fi.uba.ar>
2023-09-25 12:24:20 -0300

    Modularización Vistas

--- src/main/VistaItem.java
+++ src/main/VistaItem.java
@@ -2,16 +2,18 @@ package src.main;
 
 public class VistaItem extends VistaJuego{
 
-    // Devuelve la cantidad de items con usos restantes
+    // Devuelve la cantidad de items con al menos un uso restante
     public int mostrarItems(Entrenador entrenador) {
         System.out.println("Selecciona una opcion");
         int i = 0;
         for (Item item : entrenador.getItems()) {
             if (item.obtenerCantidad() > 0) {
                 i++;
-                System.out.println(i + " : " + item.obtenerNombre() + " (Cantidad: " + item.obtenerCantidad() + ")");
+                System.out.println(i + ": " + item.obtenerNombre() + " (Cantidad: " + item.obtenerCantidad() + ")");
             }
         }
+
+        System.out.println((i + 1)+ ": Volver atrás");
 
         return i;
     }
--- src/main/VistaPokemon.java
+++ src/main/VistaPokemon.java
@@ -4,41 +4,30 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class VistaPokemon extends VistaJuego{
-
-    // Devuelve la cantidad de Pokemones vivos
-    public int mostarPokemones(Entrenador entrenador) {
-        List<Pokemon> muertos =  new LinkedList<Pokemon>();
-        int i = 0;
-
-        System.out.println("Selecciona una opcion:");
-
-        for (Pokemon pokemon : entrenador.obtenerPokemones()) {
-            if (pokemon.obtenerEstado() == estados.MUERTO) {
-                muertos.add(pokemon);
-            } else {
-                i++;
-                System.out.println(i + ":");
-                infoPokemon(pokemon, true);
-            }
-        }
-
-        System.out.println((i + 1)+ ": Volver atrás");
-
-        System.out.println("---------------");
-        System.out.println("MUERTOS (no seleccionables): ");
-        for (Pokemon pokemon : muertos) {
-            infoPokemon(pokemon, true);
-        }
-
-        return i;
-    }
-
     public void mostarCampo(Entrenador entrenadorActual, Entrenador entrenadorRival) {
         System.out.println("Rival: " + entrenadorRival.getNombre());
         infoPokemon(entrenadorRival.obtenerPokemonActual(), false);
 
         System.out.println("Tu: " + entrenadorActual.getNombre());
         infoPokemon(entrenadorActual.obtenerPokemonActual(), true);
+    }
+
+    // Devuelve la cantidad de Pokemones
+    public int mostarPokemones(Entrenador entrenador) {
+        List<Pokemon> muertos =  new LinkedList<Pokemon>();
+        int i = 0;
+
+        System.out.println("Seleccione una opcion:");
+
+        for (Pokemon pokemon : entrenador.obtenerPokemones()) {
+                i++;
+                System.out.println(i + ":");
+                infoPokemon(pokemon, true);
+        }
+
+        System.out.println((i + 1)+ ": Volver atrás");
+
+        return i;
     }
 
     public void infoPokemon(Pokemon pokemon, Boolean mostrarAtaqueDefensa) {
