d90ef41be7bab009426b276775ac20b9315414f1
Franco Rodriguez <frodriguez@fi.uba.ar>
2023-10-28 19:06:19 -0300

    Agrego unos test de pokemon

--- src/main/Tests/PokemonTests.java
+++ src/main/Tests/PokemonTests.java
@@ -17,7 +17,6 @@ public class PokemonTests {
 
 
     @Test
-
     public void test01Pokemon() {
 
 
@@ -35,6 +34,47 @@ public class PokemonTests {
 
     }
 
+    /*
+            Pokemon envenenado muere luego de perder el %5 de su vida durante una determinada
+        cantidad de turnos consecutivos.
+            Dicha cantidad esta en funcion de su vida maxima y actual.
+    */
+    @Test
+    public void pokemonEnvenenadoMuereTest() {
+        int vidaMaxima = 20;
+        Pokemon pokemon = new Pokemon("Picachu", Tipo.RAYO, vidaMaxima, 5,
+                25, 15, "", Arrays.asList(), 5);
+        pokemon.recibirDanio(9);
+        pokemon.agregarEstado(Estados.ENVENENADO);
 
+        int vidaActual = pokemon.obtenerVidaActual();
+        int turnosHastaMorir = (int) (vidaActual / (vidaMaxima * 0.05));
 
+        if (vidaMaxima % vidaActual != 0) { turnosHastaMorir += 1; }
+
+        for (int i = 0; i < turnosHastaMorir; i++) {
+            pokemon.actualizarEstado();
+        }
+        assertEquals(true, pokemon.tieneEstado(Estados.MUERTO));
+    }
+
+    @Test
+    public void pokemonPierdeEstadosTest() {
+        Pokemon pokemon = new Pokemon("Picachu", Tipo.RAYO, 20, 5,
+                25, 15, "", Arrays.asList(), 5);
+
+        pokemon.agregarEstado(Estados.DORMIDO);
+        pokemon.agregarEstado(Estados.PARALIZADO);
+        pokemon.agregarEstado(Estados.ENVENENADO);
+
+        pokemon.removerEstado(Estados.PARALIZADO);
+        assertEquals(false, pokemon.tieneEstado(Estados.PARALIZADO));
+
+        pokemon.removerEstado(Estados.DORMIDO);
+        assertEquals(false, pokemon.tieneEstado(Estados.DORMIDO));
+
+        pokemon.removerEstado(Estados.ENVENENADO);
+        assertEquals(false, pokemon.tieneEstado(Estados.ENVENENADO));
+        assertEquals(true, pokemon.tieneEstado(Estados.NORMAL));
+    }
 }
