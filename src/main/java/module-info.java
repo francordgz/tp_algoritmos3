f71c53159474755a63160800deb919d7fd87cd42
Franco Rodriguez <frodriguez@fi.uba.ar>
2023-11-11 19:27:12 -0300

    Modifico dependencias que se rompieron

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module src.main {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
+    requires org.junit.jupiter.api;
+    requires org.mockito;
     opens src.main to javafx.fxml;
     exports src.main;
 }
