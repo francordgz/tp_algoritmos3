fa6fd6a546145795e67b7211504107b38aa51462
EzequielLassalle <ezelassalle9@gmail.com>
2023-11-27 19:56:13 -0300

    titulo

--- src/main/MainApplication.java
+++ src/main/MainApplication.java
@@ -13,7 +13,7 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Subanle un punto a Eze");
+        primaryStage.setTitle("POKEMON ES LA BATALLA");
         primaryStage.setResizable(false);
         primaryStage.show();
 
